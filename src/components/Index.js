1fa16abc87603077c35c8c8d0a856a3d2e4f63f0
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-03-28 11:14:55 +0700

    Membuat Data Baru, dan me looping nya

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,19 +1,40 @@
-import Data from './Data'
-import Datas from '../App';
+// import Data from './Data'
+// import Data from '../App';
+import data from './DataBaru';
 
-const MyComponents = props => {
+function Index() {
     return (
         <center>
-        <div className="databorder">
-        <img src={Data.album.images[0].url} className="datalogo" alt='' />
-          <div className='datatext'>
-              <p>{Data.album.name}</p>
-              <p>{Data.album.artists[0].name}</p>
-          </div>
-          <button type="button" className='btn'>Tambah Playlist</button>
+        <div className="">
+            {data.map((data, index) => {
+                return (
+                    <div key={index} className='databorder'>
+                        <img src={data.album.images[0].url} className="datalogo" alt='' />
+                        <p>{data.album.name}</p>
+                        <p>{data.album.artists.name}</p>
+                        <button type="button" className='btn'>Tambah Playlist</button>
+                    </div>
+                )
+            })}
         </div>
         </center>
     )
 }
+export default Index;
 
-export default MyComponents;
+// fuction MyComponents = props => {
+//     return (
+//         <center>
+//         <div className="databorder">
+//         <img src={Data.album.images[0].url} className="datalogo" alt='' />
+//           <div className='datatext'>
+//               <p>{Data.album.name}</p>
+//               <p>{Data.album.artists[0].name}</p>
+//           </div>
+//           <button type="button" className='btn'>Tambah Playlist</button>
+//         </div>
+//         </center>
+//     )
+// }
+
+// export default MyComponents;
